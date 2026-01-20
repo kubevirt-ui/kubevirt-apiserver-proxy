b75f7d69f3c9798d7dbcfe16788f168d42030cad
Nahshon Unna Tsameret <nunnatsa@redhat.com>
2026-01-20 14:37:42 +0200

    TLS flags refactorig
    Move the TLS flags logic to the new config package.
    
    Signed-off-by: Nahshon Unna Tsameret <nunnatsa@redhat.com>

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"crypto/tls"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
 	cache "github.com/chenyahui/gin-cache"
 	"github.com/chenyahui/gin-cache/persist"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
+	"github.com/kubevirt-ui/kubevirt-apiserver-proxy/config"
 	"github.com/kubevirt-ui/kubevirt-apiserver-proxy/handlers"
 )
 
@@ -23,37 +21,13 @@ const (
 	apiCacheTime    = 15 * time.Second
 )
 
-var (
-	minTLSVersion   uint16
-	tlsCipherSuites []uint16
-
-	minTLSVersionFlag   = flag.Uint("tls-min-version", 0, "The minimum TLS version to use")
-	tlsCipherSuitesFlag = flag.String("tls-cipher-suites", "", "A comma-separated list of cipher suites to use")
-)
-
-func init() {
+func main() {
 	flag.Parse()
 
-	if *minTLSVersionFlag > 0 {
-		minTLSVersion = uint16(*minTLSVersionFlag)
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	if *tlsCipherSuitesFlag != "" {
-		ciphers := strings.Split(*tlsCipherSuitesFlag, ",")
-		tlsCipherSuites = make([]uint16, 0, len(ciphers))
-
-		for _, cipherStr := range ciphers {
-			cipher, err := strconv.ParseUint(cipherStr, 10, 16)
-			if err != nil {
-				panic(fmt.Errorf("can't parse cipher %q; %w", cipherStr, err))
-			}
-
-			tlsCipherSuites = append(tlsCipherSuites, uint16(cipher))
-		}
-	}
-}
-
-func main() {
 
 	router := gin.Default()
 
@@ -72,17 +46,16 @@ func main() {
 		},
 	}
 
-	if minTLSVersion != 0 {
-		server.TLSConfig.MinVersion = minTLSVersion
+	if minTLSVer := cfg.GetMinTLSVersion(); minTLSVer != 0 {
+		server.TLSConfig.MinVersion = minTLSVer
 	}
 
-	if len(tlsCipherSuites) > 0 {
-		server.TLSConfig.CipherSuites = tlsCipherSuites
+	if ciphers := cfg.GetTLSCipherSuites(); len(ciphers) > 0 {
+		server.TLSConfig.CipherSuites = ciphers
 	}
 
 	log.Printf("listening for server 8080 - v0.0.10 - API cache time: %v", apiCacheTime)
 
-	var err error
 	if os.Getenv("APP_ENV") == "dev" {
 		err = server.ListenAndServe()
 	} else {
